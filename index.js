83e3c7c0863e19429a6b19da3bf1fc81f0adee1c
Irwin Ramirez <Irwinramirez23@gmail.com>
2022-05-23 05:19:29 -0400

    commit tree ff9287347630f3e0fe53e7e560dc33614f43add1

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(driversArray) {
+    let drivers = [...driversArray.slice(0, 2)];
+    return drivers;
+}
+const returnLastTwoDrivers = function(driversArray){
+    let drivers = [...driversArray.slice(driversArray.length - 2)];
+    return drivers;
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return function (fare){
+        return fare*int;
+    }
+}
+const fareDoubler = function (fare){
+    return createFareMultiplier(fare)(2);
+}
+const fareTripler = function (fare){
+    return createFareMultiplier(fare)(3);
+}
+function selectDifferentDrivers(driversArray, another) {
+    return another(driversArray);
+}
